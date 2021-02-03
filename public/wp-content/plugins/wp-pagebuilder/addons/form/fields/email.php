88726c3aa006b6cf185b629baf644eea5c565dba
taisin506 <76686960+taisin506@users.noreply.github.com>
2021-02-03 16:01:28 +0800

    1

--- public/wp-content/plugins/wp-pagebuilder/addons/form/fields/email.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
-} ?>
-
-<span class="wppb-form-field-item wppb-form-field-email">
-    <input type="email" name="wppb_default_form[<?php echo $fieldIndex; ?>]" <?php echo $fieldAttr; ?>>
-</span>
--- public/wp-content/plugins/wp-pagebuilder/addons/form/fields/textarea.php
+++
@@ -1,7 +0,0 @@
-<?php
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
-}?>
-<span class="wppb-form-field-item wppb-form-field-textarea">
-    <textarea name="wppb_default_form[<?php echo $fieldIndex; ?>]" <?php echo $fieldAttr; ?> ></textarea>
-</span>
