88e252afca1c95135dbae059ee9ce5abb5929cf6
taisin506 <76686960+taisin506@users.noreply.github.com>
2021-02-05 02:04:36 +0800

    1

--- public/wp-content/plugins/age-gate/admin/partials/language/language-hidden.php
+++
@@ -1,6 +0,0 @@
-<?php echo form_hidden('ag_'. $prefix .'['.$field.']', $values[$field]); ?>
-<?php if (self::$language): ?>
-  <?php foreach (self::$language->languages as $code => $lang): ?>
-    <?php echo form_hidden('ag_'. $prefix .'[lang]['.$code .']['.$field.']', $values['lang'][$code][$field]); ?>
-  <?php endforeach; ?>
-<?php endif; ?>
--- public/wp-content/plugins/featured-image-from-url/includes/jetpack.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-function fifu_debug_jetpack() {
-    return defined('FIFU_DEV_DEBUG') && !defined('IS_WPCOM') && FIFU_DEV_DEBUG && fifu_is_local() && !fifu_is_in_editor();
-}
-
